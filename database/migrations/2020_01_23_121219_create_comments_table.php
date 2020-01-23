c9077ee90101f4d33a606cfa481467ff1780149f
Felix Wetell <felix.wetell@gmail.com>
2020-01-23 15:32:24 +0100

    Adds comment model with migration

---
+++ database/migrations/2020_01_23_121219_create_comments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements( 'id' );
+            $table->string( 'name' );
+            $table->string( 'email' );
+            $table->ipAddress('ip');
+            $table->string( 'title' );
+            $table->longText( 'text' );
+            $table->integer( 'points' );
+            $table->foreign('post_id')
+                ->references('id')->on('posts')
+                ->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
