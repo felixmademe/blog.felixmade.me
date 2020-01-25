fd985f1c46ec826c40befd34d219b8f357205c21
Felix Wetell <felix.wetell@gmail.com>
2020-01-25 15:16:24 +0100

    Adds sluggable options to models

--- app/Post.php
+++ app/Post.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Post extends Model
 {
+    use HasSlug;
+
     /**
      * The attributes that are mass assignable.
      *
@@ -28,5 +32,28 @@ class Post extends Model
     public function categories()
     {
         return $this->hasMany( 'categories' );
+    }
+
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug')
+            ->usingSeparator('-')
+            ->slugsShouldBeNoLongerThan(50);
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 }
