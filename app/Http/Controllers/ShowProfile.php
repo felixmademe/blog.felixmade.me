530ef1a9d0841c4d137da226be66b1452a689998
Felix Wetell <felix.wetell@gmail.com>
2020-02-05 15:22:15 +0100

    Adds logic to show profile controller

---
+++ app/Http/Controllers/ShowProfile.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ShowProfile extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param Request $request
+     * @return Factory|View
+     */
+    public function __invoke(Request $request)
+    {
+        $posts = Post::where( 'state', '!=', 'public' )->get();
+        $private = $posts->where( 'state', 'private' );
+        $drafts = $posts->where( 'state', 'draft' );
+        $post = Post::latest()->first();
+
+        return view( 'user.profile' )
+            ->with( 'post', $post )
+            ->with( 'private', $private )
+            ->with( 'drafts', $drafts );
+    }
+}
