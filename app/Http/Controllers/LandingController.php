90899448ab20f4b85b4c102ac8c8ff2dc99b35c7
Felix Wetell <felix.wetell@gmail.com>
2020-02-05 15:22:05 +0100

    Adds logic to landing controller

---
+++ app/Http/Controllers/LandingController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class LandingController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param Request $request
+     * @return Factory|View
+     */
+    public function __invoke(Request $request)
+    {
+        $posts = Post::orderBy('id', 'desc')->where( 'state', 'public' )->take( 10 )->get();
+        return view( 'landing' )->with( 'posts', $posts );
+    }
+}
