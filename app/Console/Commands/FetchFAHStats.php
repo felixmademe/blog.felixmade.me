0f455459aed8a8eb54a37c57910b496c7ecf0b09
Felix Wetell <felix.wetell@gmail.com>
2020-03-27 11:37:54 +0100

    Creates command to fetch FAH Stats

---
+++ app/Console/Commands/FetchFAHStats.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Illuminate\Console\Command;
+
+class FetchFAHStats extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fetch:fahstats';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetches Folding @ Home Stats';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $url = 'https://stats.foldingathome.org/api/donor/felixwetell';
+        $content = file_get_contents( $url );
+        $json = json_decode( $content );
+
+        $user = User::first();
+        $user->fah_score = $json->credit;
+        $user->fah_rank = $json->rank;
+        $user->fah_total_users = $json->total_users;
+        $user->save();
+
+        $this->info( 'FAH Stats fetched and updated successfully!' );
+    }
+}
