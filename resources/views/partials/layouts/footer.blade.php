c139bc776373b59d284475987c4f3ff77844201e
Felix Wetell <felix.wetell@gmail.com>
2021-02-01 20:31:23 +0100

    Adds link class to footer

--- resources/views/partials/layouts/footer.blade.php
+++ resources/views/partials/layouts/footer.blade.php
@@ -5,5 +5,5 @@
             {{ env( 'APP_NAME' ) }}
         </a>
     </h4>
-    <p class="footer-copyright">Copyright @ {{ now()->year }} <a href="https://felixmade.me/">felix made me</a></p>
+    <p class="footer-copyright">Copyright @ {{ now()->year }} <a class="link" href="https://felixmade.me/">felix made me</a></p>
 </footer>
