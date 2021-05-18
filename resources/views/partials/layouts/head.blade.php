a330db8ebb06e7f2fd848a649e04e2b8c288a799
Felix Wetell <felix.wetell@gmail.com>
2021-05-18 03:47:38 +0200

    Laravel upgrade

--- resources/views/partials/layouts/head.blade.php
+++ resources/views/partials/layouts/head.blade.php
@@ -3,7 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 {{-- SEO --}}
-<title>{{ env( 'APP_NAME' ) }} - @yield( 'title', 'En blogg om inget' )</title>
+<title>{{ config( 'app.name' ) }} - @yield( 'title', 'En blogg om inget' )</title>
 <meta name="title" content="Wetell blogg - En blogg om inget">
 <meta name="description" content="Hej, jag heter Felix Wetell!Det är jag som har skapat, uppdaterar och underhåller den här bloggen.">
 <meta name="keywords" content="blogg, wetell, personal, development, coding, inlägg">
@@ -23,7 +23,6 @@
 <meta name="theme-color" content="#1a1a1a">
 
 {{-- CSS --}}
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
 <link href="{{ asset( 'css/app.css' ) }}" rel="stylesheet">
 
 {{-- JavaScript --}}
--- routes/web.php
+++ routes/web.php
@@ -11,29 +11,38 @@
 |
 */
 
-Route::get( '/', 'LandingController' );
+use App\Http\Controllers\AboutMe;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LandingController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ShowProfile;
 
-Route::get('inloggning', 'Auth\LoginController@showLoginForm')->name( 'login-form' );
-Route::post('login', 'Auth\LoginController@login')->name( 'login' );
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get( '/', LandingController::class );
+
+Route::get('inloggning', [LoginController::class, 'showLoginForm'])->name( 'login-form' );
+Route::post('login', [LoginController::class, 'login'])->name( 'login' );
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::get( 'om/bloggen', function()
 {
     return view( 'about.blog' );
 } )->name( 'about.blog' );
 
-Route::get( 'om/mig', 'AboutMe' )->name( 'about.me' );
+Route::get( 'om/mig', AboutMe::class)->name( 'about.me' );
 
 Route::group( [ 'middleware' => 'auth' ], function()
 {
-    Route::get( 'inlägg/skapa', 'PostController@create' )->name( 'posts.create' );
-    Route::get( 'inlägg/{id}/visa', 'PostController@show' )->name( 'posts.show' );
-    Route::get( 'inlägg/{id}/redigera', 'PostController@edit' )->name( 'posts.edit' );
-    Route::post( 'inlägg/lagra', 'PostController@store' )->name( 'posts.store' );
-    Route::patch( 'inlägg/{id}', 'PostController@update' )->name( 'posts.update' );
-    Route::delete( 'inlägg/{id}', 'PostController@destroy' )->name( 'posts.destroy' );
-    Route::get( 'profil', 'ShowProfile' )->name( 'user.profile' );
+    Route::get( 'inlägg/skapa', [PostController::class, 'create'] )->name( 'posts.create' );
+    Route::get( 'inlägg/{id}/visa', [PostController::class, 'show'] )->name( 'posts.show' );
+    Route::get( 'inlägg/{id}/redigera', [PostController::class, 'edit'] )->name( 'posts.edit' );
+    Route::post( 'inlägg/lagra', [PostController::class, 'store'] )->name( 'posts.store' );
+    Route::patch( 'inlägg/{id}', [PostController::class, 'update'] )->name( 'posts.update' );
+    Route::delete( 'inlägg/{id}', [PostController::class, 'destroy'] )->name( 'posts.destroy' );
+    Route::get( 'profil', [ShowProfile::class] )->name( 'user.profile' );
 });
 
-Route::get( 'inlägg', 'PostController@index' )->name( 'posts.index' );
-Route::get( 'inlägg/{slug}', 'PostController@display' )->name( 'posts.display' );
+Route::get( 'inlägg', [PostController::class, 'index'] )->name( 'posts.index' );
+Route::get( 'inlägg/{slug}', [PostController::class, 'display'] )->name( 'posts.display' );
