c63f8f561e603342e954d844c37db590fe8a5e2c
Felix Wetell <felix.wetell@gmail.com>
2021-05-24 02:47:11 +0200

    Closes #40, makes web app in to a PWA

---
+++ public/serviceworker.js
@@ -0,0 +1,46 @@
+let staticCacheName = "pwa-v" + new Date().getTime();
+let filesToCache = [
+    '/',
+    '/css/app.css',
+    '/js/app.js',
+    // add your favicons and maskable icons
+    // and add any other resource you want to be cached
+];
+
+// Cache on install
+self.addEventListener("install", event => {
+    this.skipWaiting();
+    event.waitUntil(
+        caches.open(staticCacheName)
+            .then(cache => {
+                return cache.addAll(filesToCache);
+            })
+    )
+});
+
+// Clear cache on activate
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(cacheName => (cacheName.startsWith("pwa-")))
+                    .filter(cacheName => (cacheName !== staticCacheName))
+                    .map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
+});
+
+// Serve from Cache
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+            .catch(() => {
+                return caches.match('offline');
+            })
+    )
+});
--- resources/js/app.js
+++ resources/js/app.js
@@ -3,4 +3,6 @@ require( './analytics' );
 require( './cookieconsent' );
 require( './editor' );
 require( './mobile/navigation' );
+require( './components/sw.js' );
 
+console.log('Hello :)');
