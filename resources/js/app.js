d22ef05825e1ef0678156f1a8ec5b9a4b4e82b62
Felix Wetell <felix.wetell@gmail.com>
2021-05-25 23:51:11 +0200

    Re-organizing code

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,12 @@ window.$ = require( 'jquery' );
 
 $(document).ready(function() {
     require('tinymce');
-    require('./analytics');
-    require('./cookieconsent');
-    require('./editor');
-    require('./mobile/navigation');
+    require('./components/analytics');
+    require('./components/cookieconsent');
+    require('./components/editor');
     require('./components/sw');
+    require('./components/subscription');
+    require('./mobile/navigation');
 
     console.log('Hello :)');
 });
