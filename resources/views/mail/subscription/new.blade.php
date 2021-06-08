01833324cc6668766153d326863caaa89319a31c
Felix Wetell <felix.wetell@gmail.com>
2021-06-08 02:12:26 +0200

    Adds divider on subscription email

--- resources/views/mail/subscription/new.blade.php
+++ resources/views/mail/subscription/new.blade.php
@@ -7,7 +7,7 @@
         <br>
         När jag uppdaterar bloggen kommer du vara en av de som får en uppdatering direkt, fränt va?
         Du kan när som helst avprenumerera genom att följa länken nedan.
-        <br>
+        <hr>
         <small>
             Vill inte inte längre ha dessa mail?
             <br>
